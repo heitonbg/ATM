d71cb8224c9550ded1ae7242ea6c1717cf10e2a3
iwrawww@gmail.com <iwrawww@gmail.com>
2025-05-23 18:07:59 +0700

    Добавлен метод CreateAccount для создания новых счетов

--- ATM.cs
+++ ATM.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 
 namespace ATM
 {
-    public sealed class ATM
+  public sealed class ATM
+  {
+    private static readonly Lazy<ATM> _instance = new Lazy<ATM>(() => new ATM());
+    public static ATM Instance => _instance.Value;
+
+    private Dictionary<string, decimal> _accounts;
+
+    private ATM()
     {
-        private static readonly Lazy<ATM> _instance = new Lazy<ATM>(() => new ATM());
-        public static ATM Instance => _instance.Value;
-
-        private Dictionary<string, decimal> _accounts;
-
-        private ATM()
-        {
-            _accounts = new Dictionary<string, decimal>();
-        }
+      _accounts = new Dictionary<string, decimal>();
     }
+
+    public void CreateAccount(string accountNumber, decimal initialBalance = 0)
+    {
+      if (_accounts.ContainsKey(accountNumber))
+      {
+        throw new Exception("Счет с таким номером уже существует.");
+      }
+
+      _accounts[accountNumber] = initialBalance;
+    }
+  }
 }
+    
